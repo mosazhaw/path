428d777a0ef2f39afdff7c060bb89791090a8638
Adrian Moser <trekking09@gmail.com>
2016-06-02 11:49:57 +0200

    order refactored

--- app/path-framework/path.ts
+++ app/path-framework/path.ts
@@ -3,6 +3,27 @@ export interface PathApp {
     setCurrentPage(d:String);
 }
 
+export class Page {
+    private _title:String;
+    private _content:PageElement[] = [];
+
+    get title():String {
+        return this._title;
+    }
+
+    set title(value:String) {
+        this._title = value;
+    }
+
+    get content():PageElement[] {
+        return this._content;
+    }
+
+    set content(value:PageElement[]) {
+        this._content = value;
+    }
+}
+
 export class PageElement {
     private _app:PathApp;
     private _name:String;
@@ -45,24 +66,51 @@ export class List extends PageElement {
     }
 }
 
-export class Page {
-    private _title:String;
-    private _content:PageElement[] = [];
+export class PageButton extends PageElement {
+    private _icon:String;
+    private _color:String;
+    private _page:String;
+    private _form:String;
 
-    get title():String {
-        return this._title;
+    public onClick() {
+        if (this._page != null) {
+            this.app.setCurrentPage(this._page);
+        }
+        if (this._form != null) {
+            this.app.setCurrentForm(this._form);
+        }
     }
 
-    set title(value:String) {
-        this._title = value;
+    get icon():String {
+        return this._icon;
     }
 
-    get content():PageElement[] {
-        return this._content;
+    set icon(value:String) {
+        this._icon = value;
     }
 
-    set content(value:PageElement[]) {
-        this._content = value;
+    get color():String {
+        return this._color;
+    }
+
+    set color(value:String) {
+        this._color = value;
+    }
+
+    get page():String {
+        return this._page;
+    }
+
+    set page(value:String) {
+        this._page = value;
+    }
+
+    get form():String {
+        return this._form;
+    }
+
+    set form(value:String) {
+        this._form = value;
     }
 }
 
@@ -178,52 +226,4 @@ export class AutoComplete extends FormField {
         this.filteredList = [];
     }
 
-}
-
-export class PageButton extends PageElement {
-    private _icon:String;
-    private _color:String;
-    private _page:String;
-    private _form:String;
-
-    public onClick() {
-        if (this._page != null) {
-            this.app.setCurrentPage(this._page);
-        }
-        if (this._form != null) {
-            this.app.setCurrentForm(this._form);
-        }
-    }
-
-    get icon():String {
-        return this._icon;
-    }
-
-    set icon(value:String) {
-        this._icon = value;
-    }
-
-    get color():String {
-        return this._color;
-    }
-
-    set color(value:String) {
-        this._color = value;
-    }
-
-    get page():String {
-        return this._page;
-    }
-
-    set page(value:String) {
-        this._page = value;
-    }
-
-    get form():String {
-        return this._form;
-    }
-
-    set form(value:String) {
-        this._form = value;
-    }
 }
