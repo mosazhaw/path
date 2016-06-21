01c3a2c9b6b1a894fc657e68141c5efcb98339dc
Adrian Moser <trekking09@gmail.com>
2016-06-21 09:57:47 +0200

    node server added for heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
