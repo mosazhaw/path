8f244533b0b7a0e724f3b7f37a2728b749549ce8
Adrian Moser <trekking09@gmail.com>
2016-07-15 11:34:23 +0200

    remove express basic auth

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
 var express = require('express');
-var basicAuth = require('basic-auth');
 var app = express();
-
-// simple basic authentication
-var auth = function(request, response, next) {
-    function unauthorized(response) {
-        response.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-        return response.send(401);
-    };
-
-    var user = basicAuth(request);
-
-    if (!user || !user.name || !user.pass) {
-        return unauthorized(response);
-    };
-
-    if (user.name === 'deloitte' && user.pass === 'zHn790ppp' || user.name === 'tom' && user.pass === 'skyr') {
-        return next();
-    } else {
-        return unauthorized(response);
-    };
-};
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
-app.use('/', [auth, express.static(__dirname + '/')]);
+app.use('/', [express.static(__dirname + '/')]);
 
 // set the home page route
 app.get('/', function(req, res) {
