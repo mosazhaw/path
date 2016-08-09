b518c39b61c7fde14c86aad065fe08fd1b29a9a5
Adrian Moser <trekking09@gmail.com>
2016-08-09 11:11:20 +0200

    Updated to Angular 2.0.0-rc.4

--- systemjs.config.js
+++ systemjs.config.js
@@ -27,10 +27,26 @@
     'router-deprecated',
     'upgrade',
   ];
+
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
+
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+  }
+
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
   // Add package entries for angular packages
-  ngPackageNames.forEach(function(pkgName) {
-    packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-  });
+  ngPackageNames.forEach(setPackageConfig);
+
+  // No umd for router yet
+  packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
+
   var config = {
     map: map,
     packages: packages
