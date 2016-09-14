ae76512301ccdf771dc25958b7839e9126e9a1a9
Adrian Moser <trekking09@gmail.com>
2016-09-14 11:50:33 +0200

    fix unit

--- app/gui-model/guimodel.ts
+++ app/gui-model/guimodel.ts
@@ -1474,7 +1474,7 @@ export class GuiModel {
                             "type": "autocomplete",
                             "name": "Unit",
                             "wordSearchEnabled": false,
-                            "required": true,
+                            "required": false,
                             "readonly": true,
                             "actions": [],
                             "url": "/unit",
