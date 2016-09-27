8b2f335c4c12fcca5a31b644799b4bbcbbfa3207
Adrian Moser <trekking09@gmail.com>
2016-09-27 16:16:23 +0200

    set version 0.0.2

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.0.2-SNAPSHOT";
+        return "0.0.2";
     }
 
     protected getStartPage():string {
