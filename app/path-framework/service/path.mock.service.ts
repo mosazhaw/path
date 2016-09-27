31912e8b91e8e6b6e75cd3ab4db442a904d1633c
Adrian Moser <trekking09@gmail.com>
2016-09-27 15:54:35 +0200

    fix ts compile error

--- app/path-framework/service/path.mock.service.ts
+++ app/path-framework/service/path.mock.service.ts
@@ -1,12 +1,13 @@
 import {Injectable, Inject} from "@angular/core";
 import {Http} from "@angular/http";
 import {PathService} from "./path.service";
+import {TranslationService} from "./translation.service";
 
 @Injectable()
 export class PathMockService extends PathService {
 
-    constructor(@Inject(Http) http:Http) {
-        super(http);
+    constructor(@Inject(Http) http:Http, translationService:TranslationService) {
+        super(http, translationService);
     }
 
     serverRequest(server:string, url:string, processor:(data:any) => any) {
