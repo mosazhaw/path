b8c7ff5b82f418c9c9e6ddd21161e7f43373097d
Adrian Moser <trekking09@gmail.com>
2016-10-04 07:54:05 +0200

    Release 0.0.4

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.0.4-SNAPSHOT";
+        return "0.0.4";
     }
 
     protected getStartPage():string {
