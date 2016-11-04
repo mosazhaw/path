c25d8fba07d59f7bb384a4d5298b58d0b691ff85
Adrian Moser <trekking09@gmail.com>
2016-11-04 15:39:31 +0100

    prepare version 0.0.6

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent extends path.PathAppComponent {
     }
 
     protected getFrontendVersion():string {
-        return "0.0.5";
+        return "0.0.6-SNAPSHOT";
     }
 
     protected getStartPage():string {
