b2a8f96d744afe048e6d9c72a89d23ca38a38cae
Adrian Moser <trekking09@gmail.com>
2018-06-13 08:34:53 +0200

    fix dynamic list reload issue, keep search text when back button is used

--- app/path-framework/path-app.component.ts
+++ app/path-framework/path-app.component.ts
@@ -111,14 +111,16 @@ export abstract class PathAppComponent implements path.IPathApp {
     public refreshCurrentPage() {
         for (let element of this._pageStack[this._pageStack.length - 1].content) {
             if (element instanceof path.List) {
-                (<path.List>element).refresh(null);
+                let list:path.List = <path.List>element;
+                list.filter();
             }
         }
         // breadcrumbs
         if (this._pageStack[this._pageStack.length - 2] != null) {
             for (let element of this._pageStack[this._pageStack.length - 2].content) {
                 if (element instanceof path.List) {
-                    (<path.List>element).refresh(null);
+                    let list:path.List = <path.List>element;
+                    list.filter();
                 }
             }
         }
