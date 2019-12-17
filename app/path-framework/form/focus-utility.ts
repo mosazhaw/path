8e6844b05f78c999c9da1e94b15944d76184feff
innovad <mosa@zhaw.ch>
2019-12-17 12:28:03 +0100

    fix form focus

--- app/path-framework/form/focus-utility.ts
+++ app/path-framework/form/focus-utility.ts
@@ -6,26 +6,25 @@ export class FocusUtility {
         window.setTimeout(() => {
             let focusDone = false;
 
-            let forms = <any>document.forms;
-            if (currentForm != null) {
-                forms = [forms[forms.length - 1]]; // only last form on form stack
-            }
+            const forms = <any>document.forms;
             for (const form of forms) {
-                for (const element of <any>form.elements) {
-                    if (element instanceof HTMLInputElement &&
-                        ((<HTMLInputElement>element).type === "text" ||
-                         (<HTMLInputElement>element).type === "number" ||
-                         (<HTMLInputElement>element).type === "textarea")) {
-                        const input = <HTMLInputElement>element;
-                        if (input.outerHTML.indexOf("readonly-with-required") < 0) { // no focus on readonly fields
-                            input.focus();
-                            focusDone = true;
+                if (currentForm == null || form.name !== "search") {
+                    for (const element of <any>form.elements) {
+                        if (element instanceof HTMLInputElement &&
+                            ((<HTMLInputElement>element).type === "text" ||
+                                (<HTMLInputElement>element).type === "number" ||
+                                (<HTMLInputElement>element).type === "textarea")) {
+                            const input = <HTMLInputElement>element;
+                            if (input.outerHTML.indexOf("readonly-with-required") < 0) { // no focus on readonly fields
+                                input.focus();
+                                focusDone = true;
+                            }
+                            break;
                         }
+                    }
+                    if (focusDone) {
                         break;
                     }
-                }
-                if (focusDone) {
-                    break;
                 }
             }
         }, 1);
