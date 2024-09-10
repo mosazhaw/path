9ec37782eede3bc816b5d58f6d5153aa00b3a4a5
mosazhaw <mosa@zhaw.ch>
2024-09-10 09:38:01 +0200

    release beta.4

--- build-web-component.js
+++ build-web-component.js
@@ -5,6 +5,10 @@ const VERSION = process.env.npm_package_version;
 
 build = async () => {
     await fs.emptyDir('./dist/release');
+
+    const VERSION = process.env.npm_package_version;
+    fs.createFile("./dist/release/"+ VERSION);
+
     await fs.copyFile("package.publish.json","./dist/release/package.json");
     await fs.copyFile("README.md","./dist/release/README.md");
     await fs.copyFile("CHANGELOG.md","./dist/release/CHANGELOG.md");
@@ -14,5 +18,10 @@ build = async () => {
     await fs.copyFile('./dist/path-framework/browser/styles.css','./dist/release/styles.css')
     await fs.copyFile('./dist/path-framework/browser/polyfills.js','./dist/release/polyfills.js')
     await fs.copyFile('./dist/path-framework/browser/main.js','./dist/release/main.js')
+
+    console.log("**********************************************");
+    console.log("release " + VERSION + " built in dist/release");
+    console.log("use npm publish command in direcotry dist/release");
+    console.log("**********************************************");
 }
 build();
